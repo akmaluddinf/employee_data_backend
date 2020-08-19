8a7e0d7fc12605167eaddfad4b2b80be747abc07
Akmaluddin <akmaluddinfadhilah@gmail.com>
2020-08-19 11:49:39 +0700

    create db connection

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'employee_data',
+    password: 'employee_data'
+})
+
 const by_name = (req,res)=>{
     const data = {
         name: req.params.name
@@ -5,6 +15,14 @@ const by_name = (req,res)=>{
     res.render('user_name', data)
 }
 
+const by_id = (req,res)=>{
+    sequelize.authenticate().then(
+        () => res.send('Successfull'),
+        () => res.send('Unsuccessfull')
+    )
+}
+
 module.exports ={
-    by_name
+    by_name,
+    by_id
 }
