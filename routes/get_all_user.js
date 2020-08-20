ef77c485db6fcd32aaf4b7c54954b5f75017fda8
Akmaluddin <akmaluddinfadhilah@gmail.com>
2020-08-20 17:19:21 +0700

    add validation get_all_user

--- routes/get_all_user.js
+++ routes/get_all_user.js
@@ -1,10 +1,19 @@
 const User = require('../models/user')
+const { Sequelize } = require('sequelize')
 
-const get_all_user = (req, res) =>{
-    User.findAll().then(
-        (users) => res.render('user_all', {users}),
-        (err) => res.send(err)
-    )
+const get_all_user = (req, res) => {
+  User.findAll().then(
+    (users) => {
+      //if(req.xhr){
+      if (req.headers.accept === 'application/json') {
+        res.send(users)
+      }
+      else {
+        res.render('user_all', { users })
+      }
+    },
+    (err) => res.send(err)
+  )
 }
 
 module.exports = get_all_user
